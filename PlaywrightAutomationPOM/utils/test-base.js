ab3f813c8546151e5c7497881690f96a561d24d8
R Thippeswamy Rangaswamy (Harman Connected Services Inc) <v-rrangaswam@microsoft.com>
2026-06-30 23:22:10 +0530

    first commit

--- PlaywrightAutomationPOM/utils/test-base.js
+++
@@ -1,9 +0,0 @@
-const base = require('@playwright/test')
-
-exports.customdata = base.test.extend({
-    logindata:{
-        username: 'rahulshettyacademy',
-        password: 'Learning@830$3mK2'
-    }
-
-})
